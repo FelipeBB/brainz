5a3fce3fdd94a1c0b06c5211600513e7152fe051
FelipeBB <fe.belineb@gmail.com>
2016-06-29 19:45:58 -0300

    +tests

--- test/core-test.js
+++ test/core-test.js
@@ -10,6 +10,7 @@ describe('tests', function() {
         var expiration = (new Date().getTime())*2;
         var cache = { '100': [200, expiration] };
         var brainz = new Brainz(cache);
+
         brainz('100', function(callback) {
             fakeAjax(100, callback);
         }, function(err, result) {
@@ -23,6 +24,7 @@ describe('tests', function() {
         var expiration = (new Date().getTime())*2;
         var cache = { '100': [200, expiration] };
         var brainz = new Brainz(cache);
+
         brainz('400', function(callback) {
             fakeAjax(400, callback);
         }, function(err, result) {
@@ -34,14 +36,49 @@ describe('tests', function() {
     });
 
     it('limit', function(done) {
-        done();
+        var expiration = (new Date().getTime())*2;
+        var cache = { '100': [200, expiration] };
+        var brainz = new Brainz(cache, null, 1);
+
+        brainz('400', function(callback) {
+            fakeAjax(400, callback);
+        }, function(err, result) {
+            expect(err).to.be.null;
+            expect(result).to.be.equal(400);
+            expect(cache).to.not.include.keys('400');
+            done();
+        });
     });
 
-    it('clean cache', function(done) {
-        done();
+    it('clean cache / expiration', function(done) {
+        var expiration = (new Date().getTime());
+        var cache = { '100': [200, expiration] };
+        var brainz = new Brainz(cache, null, 1);
+
+        brainz('400', function(callback) {
+            fakeAjax(400, callback);
+        }, function(err, result) {
+            expect(err).to.be.null;
+            expect(result).to.be.equal(400);
+            expect(cache).to.not.include.keys('100');
+            expect(cache).to.include.keys('400');
+            done();
+        });
     });
 
-    it('expiration', function(done) {
-        done();
+    it('custom set function', function(done) {
+        var expiration = (new Date().getTime())*2;
+        var cache = { '100': [200, expiration] };
+        var brainz = new Brainz(cache, customSet);
+
+        brainz('400', function(callback) {
+            fakeAjax(400, callback);
+        });
+
+        function customSet(cache) {
+            expect(cache).to.include.keys('400');
+            expect(cache).to.include.keys('100');
+            done();
+        }
     });
 });
